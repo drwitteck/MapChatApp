18ba9623e93ded01777250148ae10faace4b6941
Derek Witteck <drwitteck@gmail.com>
2018-03-06 18:37:43 -0500

    Created material theme and added tabbed style.

--- app/src/main/java/com/example/derekwitteck/mapchatapp/MainActivity.java
+++ app/src/main/java/com/example/derekwitteck/mapchatapp/MainActivity.java
@@ -1,15 +1,46 @@
 package com.example.derekwitteck.mapchatapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.derekwitteck.mapchatapp.R;
+import com.example.derekwitteck.mapchatapp.PartnersFragment;
+import com.example.derekwitteck.mapchatapp.MapFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new PartnersFragment(), "Partners");
     }
 }
---
+++ app/src/main/java/com/example/derekwitteck/mapchatapp/ViewPagerAdapter.java
@@ -0,0 +1,15 @@
+package com.example.derekwitteck.mapchatapp;
+
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by derekwitteck on 3/6/18.
+ */
+
+class ViewPagerAdapter {
+    public ViewPagerAdapter(FragmentManager supportFragmentManager) {
+    }
+
+    public void addFragment(PartnersFragment partnersFragment, String partners) {
+    }
+}
